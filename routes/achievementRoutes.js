7f12274ed073c8c07e3c0f527cea66b93032e888
titan1m <og0043055@gmail.com>
2025-09-25 22:07:15 +0530

    Update achievementRoutes.js

--- routes/achievementRoutes.js
+++ routes/achievementRoutes.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const { getAchievements } = require('../controllers/achievementController');
-const { protect } = require('../middleware/authMiddleware');
+const token = localStorage.getItem('token');
+const container = document.getElementById('achievements-container');
 
-router.get('/', protect, getAchievements);
-
-module.exports = router;
+fetch('/api/achievements', { headers: { 'Authorization': `Bearer ${token}` } })
+.then(res => res.json())
+.then(data => {
+    container.innerHTML = data.achievements.map(a => {
+        const unlocked = data.userAchievements.some(u => u._id === a._id);
+        return `
+        <div class="achievement ${unlocked ? 'unlocked' : ''}">
+            <img src="${a.icon}" alt="${a.title}" />
+            <h3>${a.title}</h3>
+            <p>${a.description}</p>
+        </div>`;
+    }).join('');
+});
