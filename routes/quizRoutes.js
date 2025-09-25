b725245af4c24ae97af40b4f6c6f88f3b47b5ef8
titan1m <og0043055@gmail.com>
2025-09-25 22:14:40 +0530

    Delete routes directory

--- routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getQuestions, submitQuiz } = require('../controllers/quizController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', protect, getQuestions);
-router.post('/submit', protect, submitQuiz);
-
-module.exports = router;
