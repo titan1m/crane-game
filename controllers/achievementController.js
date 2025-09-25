59f2fb3746910e4acab28f5abbfcd5ff1ea5d1f1
titan1m <og0043055@gmail.com>
2025-09-25 22:13:39 +0530

    Delete controllers directory

--- controllers/achievementController.js
+++
@@ -1,8 +0,0 @@
-const Achievement = require('../models/Achievement');
-const User = require('../models/User');
-
-exports.getAchievements = async (req, res) => {
-    const achievements = await Achievement.find();
-    const user = await User.findById(req.user.id).populate('achievements');
-    res.status(200).json({ achievements, userAchievements: user.achievements });
-};
