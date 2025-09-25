cf8828b3b2700dd4ec707d93f20fb5a90c293579
titan1m <og0043055@gmail.com>
2025-09-25 21:55:03 +0530

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+// @desc Register new user
+exports.registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: 'Please add all fields' });
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ username, email, password });
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400).json({ message: 'Invalid user data' });
+    }
+};
+
+// @desc Login user
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
+};
+
+// @desc Get current user
+exports.getMe = async (req, res) => {
+    const user = await User.findById(req.user.id);
+    res.status(200).json(user);
+};
