6e1139e5244ed5d9d61d38eb79af079f2dbc3e98
titan1m <og0043055@gmail.com>
2025-09-25 22:06:21 +0530

    Update leaderboardRoutes.js

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const { getLeaderboard } = require('../controllers/leaderboardController');
-const { protect } = require('../middleware/authMiddleware');
+const token = localStorage.getItem('token');
+const leaderboardList = document.getElementById('leaderboard-list');
 
-router.get('/', protect, getLeaderboard);
-
-module.exports = router;
+fetch('/api/leaderboard', {
+    headers: { 'Authorization': `Bearer ${token}` }
+})
+.then(res => res.json())
+.then(data => {
+    leaderboardList.innerHTML = data.map((u, idx) => `
+        <li>${idx+1}. ${u.username} - ${u.score} pts</li>
+    `).join('');
+});
