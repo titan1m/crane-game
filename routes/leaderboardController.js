013fd26a72d4f14238a4b5db8c860716bc61c9b3
titan1m <og0043055@gmail.com>
2025-09-25 22:01:53 +0530

    Delete routes/leaderboardController.js

--- routes/leaderboardController.js
+++
@@ -1,6 +0,0 @@
-const User = require('../models/User');
-
-exports.getLeaderboard = async (req, res) => {
-    const leaderboard = await User.find().sort({ score: -1 }).limit(10).select('username score');
-    res.status(200).json(leaderboard);
-};
